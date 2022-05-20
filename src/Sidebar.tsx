6bb9690246210dc8f08b7fe0f56187dea9fa2115
Jiayu Liu <jiayu@hey.com>
2022-05-20 14:18:31 +0800

    load catalogs from be

--- src/Sidebar.tsx
+++ src/Sidebar.tsx
@@ -1,6 +1,25 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { invoke } from "@tauri-apps/api/tauri";
 import "./Sidebar.css";
 
 export default function Sidebar() {
-  return <header className="sidebar">Sidebar</header>;
+  const [catalogs, setCatalogs] = useState<string[] | null>(null);
+  async function loadCatalogs() {
+    const catalogs: string[] = await invoke("catalogs");
+    setCatalogs(catalogs);
+  }
+  useEffect(() => {
+    loadCatalogs();
+  });
+  let content = <span>Empty</span>;
+  if (catalogs !== null) {
+    content = (
+      <ul>
+        {catalogs.map((catalog) => {
+          return <li>{catalog}</li>;
+        })}
+      </ul>
+    );
+  }
+  return <header className="sidebar">{content}</header>;
 }

505f2a965b8a8423dd266177bac0ba58a6761d39
Jiayu Liu <jiayu@hey.com>
2022-05-20 10:41:08 +0800

    window layout

---
+++ src/Sidebar.tsx
@@ -0,0 +1,6 @@
+import { useState } from "react";
+import "./Sidebar.css";
+
+export default function Sidebar() {
+  return <header className="sidebar">Sidebar</header>;
+}
---
+++ src/Workspace.tsx
@@ -0,0 +1,7 @@
+import { useState } from "react";
+
+import "./Workspace.css";
+
+export default function Workspace() {
+  return <main className="workspace">Workspace</main>;
+}

d91f389ed3a45d6ff92d9c1cb558bfe67aad39fe
Jiayu Liu <Jimexist@users.noreply.github.com>
2022-05-22 15:29:50 +0800

    editor tabs (#5)
    * work editor state
    
    * add editor tabs

--- src/Workspace.tsx
+++ src/Workspace.tsx
@@ -1,9 +1,13 @@
 import { useState } from "react";
 import { Flex } from "@chakra-ui/react";
+import EditorTabs from "./EditorTabs";
+import ResultTable from "./ResultTable";
+
 export default function Workspace() {
   return (
-    <Flex as={"main"} margin={4}>
-      Workspace
+    <Flex as={"main"} margin={4} align={"stretch"} direction={"column"} gap={2}>
+      <EditorTabs />
+      <ResultTable />
     </Flex>
   );
 }

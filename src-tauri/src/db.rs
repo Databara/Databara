713de753f266a9f4543ef89afe3f80749743c023
Jiayu Liu <jiayu@hey.com>
2022-05-29 14:24:05 +0800

    fix issues and version updates

--- src-tauri/src/db.rs
+++ src-tauri/src/db.rs
@@ -6,22 +6,22 @@ use std::env;
 use std::sync::Mutex;
 
 pub struct DbState {
-  pub conn: Mutex<SqliteConnection>,
+    pub conn: Mutex<SqliteConnection>,
 }
 
 impl DbState {
-  pub fn try_new() -> Result<Self> {
-    Ok(DbState {
-      conn: Mutex::new(establish_connection()?),
-    })
-  }
+    pub fn try_new() -> Result<Self> {
+        Ok(DbState {
+            conn: Mutex::new(establish_connection()?),
+        })
+    }
 }
 
 const IN_MEMORY_URL: &str = ":memory:";
 
 fn establish_connection() -> Result<SqliteConnection> {
-  dotenv().ok();
-  let database_url = env::var("DATABASE_URL").unwrap_or(IN_MEMORY_URL.to_string());
-  let conn = SqliteConnection::establish(&database_url)?;
-  Ok(conn)
+    dotenv().ok();
+    let database_url = env::var("DATABASE_URL").unwrap_or(IN_MEMORY_URL.to_string());
+    let conn = SqliteConnection::establish(&database_url)?;
+    Ok(conn)
 }
--- src-tauri/src/models/db_state.rs
+++ src-tauri/src/models/db_state.rs
@@ -1,13 +1,11 @@
 use diesel::Queryable;
-use serde::Serialize;
 use std::time::SystemTime;
 
 #[derive(Debug, Queryable)]
-struct EditorTab {
-  pub id: i32,
-  pub title: String,
-  pub content: String,
-  pub created_at: SystemTime,
-  pub updated_at: SystemTime,
+pub struct EditorTab {
+    pub id: i32,
+    pub title: String,
+    pub content: String,
+    pub created_at: SystemTime,
+    pub updated_at: SystemTime,
 }
-
